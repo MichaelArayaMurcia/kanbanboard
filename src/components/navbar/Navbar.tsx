9614db6f9b69622a6b9803f110cff533ada6792b
MichaelArayaMurcia <michaelarayamurcia@gmail.com>
2023-04-19 20:58:28 -0600

    The navbar component has been setup, I intent on learning how to make it look better with the help of Material UI

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
+import LogoImage from "../../assets/logo-dark.svg" 
+import IconVertical from "../../assets/icon-vertical-ellipsis.svg"
+
+import "./navbar.scss"
+
 function Navbar() {
   return (
-    <div>navbar</div>
+    <div className="navbarContainer">
+        <div className="navbarLogoContainer">
+            <button><img src={LogoImage} alt="logo-dark" /></button>
+        </div>
+        <div className="navbarContentContainer">
+            <h1> Platform Launch </h1>
+            <button>+ Add New Task</button>
+            <button><img src={IconVertical} alt="IconVertical" /></button>
+        </div>
+    </div>
   )
 }
 
